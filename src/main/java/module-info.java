5e8f896a7ff6dabdb74efd7e8f9411e03845fac7
Alexzander Bond <abond@cbet.edu>
2021-07-20 09:13:19 -0500

    Added module-info to project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * One might notice that JSON isn't capitalized in the names of classes and probably methods, this is because it is easier to separate words when the have lowercase letters following uppercase ones
+ */
+module ObjectiveJSON {
+    requires org.jetbrains.annotations;
+    exports edu.cbet.json;
+    exports edu.cbet.json.annotations;
+}
